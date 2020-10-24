b72c0f43c0bea8754e2af91567d84f3414d853d4
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-10-24 11:53:10 -0700

    cleaner code

--- floodfill.cpp
+++ floodfill.cpp
@@ -1,42 +1,31 @@
+
 #include <bits/stdc++.h> 
 using namespace std;
-int di[4] = {0,0,1,-1};
-int dj[4] = {1,-1,0,0};
+int n,m;
+string arr[101];
+bool mark[101][101];
+
+void floodfill(int i, int j){
+  if (i < 0 || i >= n || j < 0 || j >= m || mark[i][j] || arr[i][j] == '*'){
+    return;
+  }
+  mark[i][j] = true;
+  floodfill(i+1,j);
+  floodfill(i-1,j);
+  floodfill(i,j+1);
+  floodfill(i,j-1);
+}
+
 int main(){
-  int h,w,itemp,jtemp,itemp2,jtemp2; cin >> h >> w;
-  char arr[h][w];
-  for (int i = 0; i < h; i++){
-    for (int j = 0; j < w; j++){
-      cin >> arr[i][j];
+  cin >> n >> m;
+  for (int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++){
       if (arr[i][j] == 'S'){
-        itemp = i;jtemp = j;
+        floodfill(i,j);
       }
     }
-  }
-  queue <int> iq;
-  queue <int> jq;
-  iq.push(itemp);
-  jq.push(jtemp);
-  arr[itemp][jtemp] = '!';
-  while (iq.size() > 0){
-    itemp = iq.front();jtemp = jq.front();
-    iq.pop();jq.pop();
-    for (int itr = 0; itr < 4; itr++){
-      itemp2 = itemp+di[itr];
-      jtemp2 = jtemp+dj[itr];
-      if (itemp2 >= 0 && itemp2 < h && jtemp2 >= 0 && jtemp2 < w){
-        if (arr[itemp2][jtemp2] == '.'){
-          arr[itemp2][jtemp2] = '!';
-          iq.push(itemp2);
-          jq.push(jtemp2);
-        }
-      }
-    }
-  }
-  for (int i = 0; i < h; i++){
-    for (int j = 0; j < w; j++){
-      cout << arr[i][j];
-    }
-    cout << "" << endl;
   }
 }
