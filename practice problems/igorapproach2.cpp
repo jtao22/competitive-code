e4b024e34d74069dd119be61e150d076cd327ab5
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-10 16:11:03 -0800

    optimized, passed

--- practice problems/igorapproach2.cpp
+++ practice problems/igorapproach2.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 int di[4] = {0,0,-1,1};
 int dj[4] = {1,-1,0,0};
-int ind[30000];
+long ind[1000000];
+char c;
+long mark[1001][1001];
 queue <int> qi;queue <int> qj;
+vector<int>wi;vector<int>wj;
 int main(){
-  int h,w,q,bh,bw,itemp,jtemp,itemp2,jtemp2,indcount,count =0,currindex = 1; cin >> h >> w >> q; bool found = false;
-  char arr[h][w];
-  int mark[h][w];
+  int h,w,q,bh,bw,itemp,jtemp,itemp2,jtemp2,count = 0,currindex = 1,save = 0; cin >> h >> w >> q;
   for (int i = 0; i < h; i++){
     for (int j = 0; j < w; j++){
-      cin >> arr[i][j];
-      mark[i][j] = 0;
-      if (!found){
-        if (arr[i][j] == '.'){
-          found = true;qi.push(i);qj.push(j);
+      cin >> c;
+      if (c == '*'){
+        mark[i][j] = -1;
+      }
+      else{
+        mark[i][j] = 0;
+        wi.push_back(i);wj.push_back(j);
+        if (qi.size() ==0){
+          qi.push(i);qj.push(j);
         }
       }
     }
@@ -23,13 +28,13 @@ int main(){
       itemp = qi.front();jtemp=qj.front();
       qi.pop();qj.pop();
       mark[itemp][jtemp] = currindex; 
-      indcount = 4;
+      ind[currindex]+= 4;
       for (int itr = 0; itr < 4; itr++){
         itemp2 = itemp+di[itr];
         jtemp2 = jtemp+dj[itr];
         if (itemp2>= 0 && itemp2 < h && jtemp2 >= 0 && jtemp2 < w){
-          if (arr[itemp2][jtemp2] == '.'){
-            indcount--;
+          if (mark[itemp2][jtemp2] != -1){
+            ind[currindex]--;
             if (mark[itemp2][jtemp2] == 0){
               qi.push(itemp2); qj.push(jtemp2);
               mark[itemp2][jtemp2] = currindex;
@@ -37,19 +42,12 @@ int main(){
           }
         }
       }
-      ind[currindex]+=indcount;
       if (qi.size() == 0){
-        found = false;
-        for (int i = 0; i < h; i++){
-          for (int j = 0; j < w; j++){
-            if (arr[i][j] == '.' && mark[i][j] == 0){
-              qi.push(i);qj.push(j);
+        for (int i = save; i < wi.size(); i++){
+          if (mark[wi[i]][wj[i]] == 0){
+              save = i;
+              qi.push(wi[i]);qj.push(wj[i]);
               currindex++;
-              found = true;
-              break;
-            }
-          }
-          if (found){
             break;
           }
         }
