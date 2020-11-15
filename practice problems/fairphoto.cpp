6738d02767d1b1a62c965c058304c125affe5793
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-15 12:28:26 -0800

    redid problem, implemented maps

--- practice problems/fairphoto.cpp
+++ practice problems/fairphoto.cpp
@@ -1,49 +1,45 @@
 #include <bits/stdc++.h> 
 using namespace std;
-int n,x; char c;
-struct cows{
-  int pos = 0;
-  int breed = 0;
-  int psum = 0;
+
+struct cowinp{
+  int pos;
+  int breed;
+  int sum;
 };
-bool comp(cows a, cows b){
-  return a.pos <= b.pos;
+bool comp(cowinp a, cowinp b){
+  return a.pos < b.pos;
 }
-int occ[1100000];
+map<int,int>sumocc;
 int main(){
-  cin >> n;
-  cows cow[n+1];
+  int n; cin >> n; char c;
+  cowinp cow[n+1]; 
   for (int i = 1; i <= n; i++){
-    cin >> x >> c;
-    if (c == 'H'){
+    cin >> cow[i].pos >> c;
+    if (c == 'G'){
       cow[i].breed = 1;
     }
-    else{
-      cow[i].breed = -1;
-    }
-    cow[i].pos = x;
+    else{cow[i].breed = -1;}
   }
-  sort(cow,cow+n+1,comp);
-  occ[n] = cow[1].pos;
-  cow[0].psum = n; int best = 0; 
+  sort(cow+1,cow+n+1,comp);
+
+  cow[0].sum = 0; int longcont = 0,best = -1; cow[0].breed = cow[1].breed; cow[0].pos = cow[1].pos;
   for (int i = 1; i <= n; i++){
-    cow[i].psum = cow[i-1].psum+cow[i].breed;
-    if (occ[cow[i].psum] == 0){
-      occ[cow[i].psum] = i;
+    cow[i].sum = cow[i-1].sum+cow[i].breed;
+    if (cow[i].breed == cow[i-1].breed){
+      longcont+=cow[i].pos-cow[i-1].pos;
     }
     else{
-      best = max(best,cow[i].pos-cow[occ[cow[i].psum]+1].pos);
+      best = max(longcont,best);
+      longcont=0;
     }
-  } int prev = cow[1].breed, count = 0; cow[0].pos = cow[1].pos;
+  }
   for (int i = 1; i <= n; i++){
-    if (cow[i].breed == prev){
-      count+=(cow[i].pos-cow[i-1].pos);
+    if (!sumocc.count(cow[i].sum)){
+      sumocc[cow[i].sum] = i;
     }
     else{
-      best = max(best,count);
-      count = 0;
+      best = max(best,cow[i].pos - cow[sumocc[cow[i].sum]+1].pos);
     }
-    prev = cow[i].breed;
   }
   cout << best << endl;
 }
