339a4653f9614440c090f9cd83fbcc600eb67772
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-15 13:16:49 -0800

    fixed errors

--- practice problems/citystate.cpp
+++ practice problems/citystate.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h> 
 using namespace std;
+map<string,int>m;
 int main(){
-  freopen("citystate.in","r",stdin);
-  freopen("citystate.out","w",stdout);
-  int n,count=0;
-  cin >> n;
-  pair <string,string> citystate[n];
-  string foo;
-  cin >> foo >> citystate[0].second;
-  citystate[0].first = foo.substr(0,2);
-  for (int i = 1; i < n; i++){
-    cin >> foo >> citystate[i].second;
-    citystate[i].first = foo.substr(0,2);
-    for (int j = i-1; j >=0; j--){
-      if (citystate[i].first == citystate[j].second && citystate[i].second == citystate[j].first){
-        count++;
-      }
+  int n,count = 0; cin >> n; string city, state;
+  map<string,int>cs;
+  for (int i = 0; i < n; i++){
+    cin >> city >> state; city = city.substr(0,2);
+    if (cs.find(state+city) != cs.end() && city != state){
+      count+=cs[state+city];
     }
+    cs[city+state]++;
   }
   cout << count << endl;
 }
