e6ff9eff168f03bbdb095750e8a1f955cd21e772
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2020-11-19 22:38:02 -0800

    practice problems

--- practice problems/lightson.cpp
+++ practice problems/lightson.cpp
@@ -1,53 +1,53 @@
 #include <bits/stdc++.h> 
 using namespace std;
+map<pair<int,int>,vector<pair<int,int>>>adj;
+bool visited[101][101];
+bool on[101][101];
+int n,tempi,tempj,lighttempi,lighttempj;
+int c[4];
 int di[4] = {0,0,1,-1};
-int dj[4] = {-1,1,0,0};
-int room[101][101];
-bool lighton[101][101];
-int n,m,x,y,a,b;
-pair <int,int> pii;
-pair <int,int> pii2;
-int main(){
-  //freopen("lightson.in","r",stdin);
-  //freopen("lightson.out","w",stdout);
-  int curri, currj;
-  map<pair<int,int>,vector <pair<int,int>>> check;
-  cin >> n >> m;
-  for (int i = 0; i < m; i++){
-    cin >> x >> y;
-    pii = make_pair(x,y);
-    cin >> a >> b;
-    pii2 = make_pair(a,b);
-    check[pii].push_back(pii2);
+int dj[4] = {1,-1,0,0};
+pair<int,int>pii;
+void dfs(int i, int j){
+  pii = make_pair(i,j);
+  for (int itr = 0; itr < adj[pii].size(); itr++){
+    if (!on[adj[pii][itr].first][adj[pii][itr].second]){
+    on[adj[pii][itr].first][adj[pii][itr].second] = true;
+    c[1]++;
+    }
   }
-  int count = 1;
-  room[1][1] = count;
-  lighton[1][1] = true;
-  queue<int>qi;
-  queue<int>qj;
-  qi.push(1);
-  qj.push(1);
-  while (qi.size() > 0){
-    curri = qi.front();
-    currj = qj.front();
-    qi.pop();
-    qj.pop();
-    pii = make_pair(curri,currj);
-    for (int i = 0; i < check[pii].size(); i++){
-      if (!lighton[check[pii][i].first][check[pii][i].second]){
-        lighton[check[pii][i].first][check[pii][i].second] = true;
-        count++;
+  for (int itr = 0; itr < 4; itr++){
+    tempi = i+di[itr];tempj = j+dj[itr];
+    if (tempi > 0 && tempi <= n && tempj > 0 && tempj <= n){
+      if (!visited[tempi][tempj] && on[tempi][tempj]){
+        visited[tempi][tempj] = true;
+        c[3]++;
+        dfs(tempi,tempj);
       }
     }
-    for (int i = 0; i < 4; i++){
-      if (curri+di[i] > 0 && currj+dj[i] > 0 && curri+dj[i] <= n && currj+dj[i] <= n){
-        if (lighton[curri+di[i]][currj+dj[i]] && room[curri+di[i]][currj+dj[i]] == 0){
-          room[curri+di[i]][currj+dj[i]] = 1;
-          qi.push(curri+di[i]);
-          qj.push(currj+dj[i]);
+  }
+}
+int main(){
+  int q,x1,y1,x2,y2,count=0; cin >> n >> q;
+  for (int i = 1; i <= q; i++){
+    cin >> x1 >> y1 >> x2 >> y2;
+    adj[make_pair(x1,y1)].push_back(make_pair(x2,y2));
+  }
+  visited[1][1] = true;
+  c[1]++;
+  c[3]++;
+  on[1][1] = true;
+  dfs(1,1);
+  while (c[0] != c[1] || c[2] != c[3]){
+    c[0] = c[1];
+    c[2] = c[3];
+    for (int i = 1; i <= n; i++){
+      for (int j = 1; j <= n; j++){
+        if (visited[i][j]){
+          dfs(i,j);
         }
       }
     }
-  }
-  cout << count+1 << endl;
+}
+ cout << c[1] << endl;
 }
