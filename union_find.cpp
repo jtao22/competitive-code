d99c554d2074e102c2feee76d14f6a3a6e0e544c
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2021-01-21 22:25:35 -0800

    algorithms

---
+++ union_find.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+int di[4] = {1,-1,0,0};
+int dj[4] = {0,0,1,-1};
+int n,m,a,b;
+int parent[10000];
+int length[1000];
+int root(int x){
+  if(parent[x] == x){
+    return x;
+  }
+  return root(parent[x]);
+}
+void join(int x, int y){
+  int rootx = root(x), rooty = root(y);
+  if(length[x] > length[y]){
+    parent[y] = x;
+  }
+  else{
+    parent[x] = y;
+    length[y] = max(length[y],length[x]+1);
+  }
+}
+int main(){
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++){
+    parent[i] = i;
+  }
+  for (int i = 0; i < m; i++){
+    cin >> a >> b;
+    join(a,b);
+  }
+}
