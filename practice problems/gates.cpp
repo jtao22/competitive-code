7f673f3a9b621bc88a2208c6964f8391d0065ebd
Jonathan Tao <68477691+jtao22@users.noreply.github.com>
2021-03-27 21:47:04 -0700

    Update gates.cpp

--- practice problems/gates.cpp
+++ practice problems/gates.cpp
@@ -1,38 +1,55 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
-int fence[1500][1500];
+typedef pair<int,int> pii;
+int di[4] = {0,0,1,-1};
+int dj[4] = {1,-1,0,0};
+bool field[2005][2005];
+int n;
+map<char,pii>direction;
 int main(){
-  //freopen("gates.in","r",stdin);
-  //freopen("gates.out","w",stdout);
-  int n,x = 750,y = 750,count = 0; cin >> n;
-  char inp;
-  fence[750][750] = 2;
+  freopen("gates.in","r",stdin);
+  freopen("gates.out","w",stdout);
+  int curri = 1002,currj = 1002; char c;
+  cin >> n;
+  direction['N'] = {-1,0};
+  direction['S'] = {1,0};
+  direction['W'] = {0,-1};
+  direction['E'] = {0,1};
+  field[curri][currj] = true;
   for (int i = 0; i < n; i++){
-    cin >> inp;
-    if (inp == 'N'){
-      y--;
-    }
-    else if (inp == 'S'){
-      y++;
-    }
-    else if (inp == 'W'){
-      x--;
-    }
-    else{
-      x++;
-    }
-    if (fence[y][x] == 0){
-      fence[y][x]++;
-    }
-    else{
-      count++;
+    cin >> c;
+    curri+=direction[c].first;
+    currj+=direction[c].second;
+    field[curri][currj] = true;
+    curri+=direction[c].first;
+    currj+=direction[c].second;   
+    field[curri][currj] = true;
+  }
+  queue<pii>q;
+
+  int count = 0;
+  for(int i = 0; i < 2005; i++){
+    for (int j = 0; j < 2005; j++){
+      if(!field[i][j]){
+        field[i][j] = true;
+        count++;
+        q.push({i,j});
+        while(!q.empty()){
+          curri = q.front().first; currj = q.front().second;
+          q.pop();
+          for (int itr = 0; itr < 4; itr++){
+            int tempi = curri+di[itr];
+            int tempj = currj+dj[itr];
+            if(tempi >= 0 && tempi < 2005 && tempj >= 0 && tempj < 2005 && !field[tempi][tempj]){
+              field[tempi][tempj] = true;
+                q.push({tempi,tempj});
+            }
+          }
+        }
+      }
     }
   }
-  for (int i = 750-n; i < 750+n; i++){
-    for (int j = 750-n; j < 750+n; j++){
-      cout << fence[i][j] << " ";
-    }
-    cout << "" << endl;
-  }
-  cout << count << endl;
+  cout << count-1 << endl;
 }
